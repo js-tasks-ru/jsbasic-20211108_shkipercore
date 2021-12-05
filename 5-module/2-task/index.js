622398eff5bfd7cbe93de675486f6e33a1ef6d1a
Shivyrtalov Nikita <shkipercore@gmail.com>
2021-12-06 02:54:07 +0700

    add solution for 5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  const toggleTextButton = document.querySelector('.toggle-text-button');
+  const div = document.querySelector('#text');
+
+  toggleTextButton.addEventListener('click', () =>
+  {
+    if (div.hidden === false) {
+      div.hidden = true;
+    } else if (div.hidden === true) {
+      div.hidden = false;
+    }
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const carousel = document.querySelector('.carousel__inner');
+  const slideWight = document.querySelector('.carousel__slide').offsetWidth;
+  const slideCount = document.querySelectorAll('.carousel__slide').length;
+
+  let currentPosition = 0;
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', () =>
+  {
+    currentPosition = currentPosition - slideWight;
+    moveCarousel();
+  });
+
+  arrowLeft.addEventListener('click', () =>
+  {
+    currentPosition = currentPosition + slideWight;
+    moveCarousel();
+  });
+
+  function moveCarousel() {
+    carousel.style.transform = `translateX(${currentPosition}px)`;
+
+    if (currentPosition === 0) {
+      arrowLeft.style.display = 'none';
+    } else {
+      arrowLeft.style.display = '';
+    }
+
+    if (Math.abs(currentPosition) === slideWight * (slideCount - 1))
+    {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowRight.style.display = '';
+    }
+  }
 }
